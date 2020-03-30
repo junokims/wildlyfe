6607232c226e91d423a36d33a01711eea00d638c
junokims <juno.jaekwon.kim@gmail.com>
2020-03-30 01:43:53 +0100

    fixed div

--- division-connect.php
+++ division-connect.php
@@ -2,30 +2,8 @@
 <body>
 
 <?php
-if (isset($_POST['submit_form'])) {
-    $someDiet = $_POST['selected_diet'];
-    if (empty($someDiet)) {
-        echo "Name is empty";
-    } else {
-        // echo $someDiet;
-        // // echo <option value='$selected_diet'>$selected_diet</option>; 
-        // $dbconnect = mysqli_connect('localhost', 'root', 'root', 'wyldlyfe')or die("initial host/db connection problem");
-        
-        // $sql = "SELECT * FROM Species"; //You don't need a ; like you do in SQL
-        // $run = mysqli_query($dbconnect, $sql);
+if (isset($_POST['save'])) {
 
-        // echo "<table>"; // start a table tag in the HTML
-        
-        // while($row = $mysqli->query($dbconnect, $sql)->fetch_array()){   //Creates a loop to loop through results
-        //     printf ("%s (%s)\n", $row["Species"]);        }
-        
-
-        // echo "</table>"; //Close the table in HTML
-
-
-
-
-        echo $someDiet;
         $dbconnect = mysqli_connect('localhost', 'root', 'root', 'wyldlyfe')or die("initial host/db connection problem");
 
 
@@ -45,21 +23,6 @@ if (isset($_POST['submit_form'])) {
         mysqli_close($dbconnect);
 
     }
-
-    // CREATE Table CanGraduate AS SELECT * FROM AllStudents 
-    // WHERE NOT EXISTS 
-    // (SELECT * FROM CannotGraduate WHERE 
-    //     CannotGraduate.Student_name = AllStudents.Student_name)
-
-
-    // $dbconnect = mysqli_connect('localhost', 'root', 'root', 'wyldlyfe')or die("initial host/db connection problem");
-
-    // $sql ="INSERT into AnimalLivesIn (Animal_ID, Enclosure_ID, Species) VALUES ('$animalid', '$enclosureid', '$species')";
-
-    // $run = mysqli_query($dbconnect, $sql)or die(mysqli_error($dbconnect));
-
-}
-
 ?>
 
 </body>
