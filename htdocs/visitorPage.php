3284b829044f252f1232f5db28c9bdfc39d9b5fa
junokims <juno.jaekwon.kim@gmail.com>
2020-04-02 23:23:57 +0100

    updated visitorPage and join-connect

--- htdocs/visitorPage.php
+++ htdocs/visitorPage.php
@@ -5,96 +5,109 @@
 <html>
 <head>
 <title>Table with database</title>
-<title>Find all entry tickets purchased from a certain employee</title>
+<title>Find information about our visitors!</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-<style>
-      html, body {
-      min-height: 100%;
-      }
-      body, div, form, input, select, p { 
-      padding: 0;
-      margin: 0;
-      outline: none;
-      font-family: Roboto, Arial, sans-serif;
-      font-size: 14px;
-      color: #666;
-      line-height: 22px;
-      }
-      h1 {
-      margin: 15px 0;
-      font-weight: 400;
-      }
-      h4 {
-      margin-bottom: 4px;
-      }
-      .testbox {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: inherit;
-      padding: 3px;
-      }
+    <style>
+html {
+  box-sizing: border-box;
+}
 
-      input, select, textarea {
-      width: 100%;
+body{
+background-image: url("wildlife8.PNG");
+border-collapse: collapse;
+width: 80%;
+color: #666;
+font-family: Roboto, Arial, sans-serif;
+font-size: 20px;
+text-align: left;
+padding: 1em 0;
+margin: 0 auto;
+max-width: 56em;
+}
+input{
+border-collapse: collapse;
+width: 80%;
+color: #666;
+font-family: Roboto, Arial, sans-serif;
+font-size: 20px;
+text-align: left;
+padding: 1em 0;
+margin: 0 auto;
+max-width: 56em;
+}
+
+table {
+border-collapse: collapse
+background-color: white;
+width: 80%;
+color: #666;
+font-family: Roboto, Arial, sans-serif;
+font-size: 20px;
+text-align: left;
+padding: 1em 0;
+margin: 0 auto;
+max-width: 56em;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+.column {
+  float: left;
+  width: 33.3%;
+  margin-bottom: 16px;
+  padding: 0 8px;
+}
+
+@media screen and (max-width: 650px) {
+  .column {
+    width: 100%;
+    display: block;
+  }
+}
+ input, select, textarea {
+      width: 25%;
       margin-bottom: 10px;
       border: 1px solid #ccc;
       border-radius: 3px;
       }
       input {
-      width: calc(100% - 10px);
+      width: calc(50%% - 10px);
       padding: 5px;
       }
       input:hover, textarea:hover, select:hover {
       outline: none;
       border: 1px solid #095484;
       }
-      select {
-      padding: 7px 0;
-      background: transparent;
-      }
-      textarea {
-      margin-bottom: 3px;
-      }
-      .item {
-      position: relative;
-      display: flex;
-      flex-direction: column;
-      margin: 10px 0;
-      }
-      input[type="date"]::-webkit-inner-spin-button {
-      display: none;
-      }
-      .item i, input[type="date"]::-webkit-calendar-picker-indicator {
-      position: absolute;
-      font-size: 20px;
-      color: #a9a9a9;
-      }
-      .item i {
-      left: 94%;
-      top: 30px;
-     z-index: 1;
-      }
-      [type="date"]::-webkit-calendar-picker-indicator {
-      left: 93%;
-      z-index: 2;
-      opacity: 0;
-      cursor: pointer;
-      }
-      .street, .desired-outcome-item, .complaint-details-item {
-      display: flex;
-      flex-wrap: wrap;
-      }
-      .street input {
-      margin-bottom: 10px;
-      }
-      small {
-      display: block;
-      line-height: 16px;
-      opacity: 0.7;
-      }
-      .btn-block {
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+}
+
+.container {
+  padding: 0 16px;
+  background-color: white;
+  border-radius: 25px;
+  border: 1px solid #AED6F1;
+}
+
+.container2 {
+  padding: 0 16px;
+  background-color: white;
+}
+
+.container2::after, .row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.title {
+  color: grey;
+}
+
+.btn-block {
       margin-top: 20px;
       text-align: center;
       }
@@ -113,52 +126,9 @@
       button:hover {
       background-color: #0666a3;
       }
-      @media (min-width: 568px) {
-      input {
-      width: calc(35% - 20px);
-      margin: 0 0 0 8px;
-      }
-      select {
-      width: calc(50% - 8px);
-      margin: 0 0 10px 8px;
-      }
-      .item {
-      flex-direction: row;
-      align-items: center;
-      }
-      .item p {
-      width: 30%;
-      }
-      .item i {
-      left: 61%;
-      top: 25%;
-      }
-      [type="date"]::-webkit-calendar-picker-indicator {
-      left: 60%;
-      }
-      .street, .desired-outcome-item, .complaint-details-item {
-      width: 70%;
-      }
-      .street input {
-      width: calc(50% - 20px);
-      }
-      .street .street-item {
-      width: 100%;
-      }
-      .address p, .desired-outcome p, .complaint-details p {
-      align-self: flex-start;
-      margin-top: 6px;
-      }
-      .desired-outcome-item, .complaint-details-item {
-      margin-left: 12px;
-      }
-      textarea {
-      width: calc(100% - 6px);
-      }
-      }
-table, body {
+table {
 border-collapse: collapse;
-width: 90%%;
+width: 80%%;
 color: #666;
 font-family: Roboto, Arial, sans-serif;
 font-size: 20px;
@@ -168,39 +138,22 @@ margin: 0 auto;
 max-width: 56em;
 }
 th {
-background-color: #588c7e;
+background-color: #0070b5;
 color: white;
 }
-
-    button {
-    width: 150px;
-    padding: 10px;
-    border: none;
-    -webkit-border-radius: 5px;
-   -moz-border-radius: 5px;
-   border-radius: 5px;
-   background-color: #095484;
-   font-size: 16px;
-   color: #fff;
-   cursor: pointer;
-    }
-   button:hover {
-   background-color: #0666a3;
-   }
-
 .button-container form, .button-container form div{
 display: inline;
 }
-
+l
 .button-container button {
 display: inline;
 vartical-align: middle;
 }
-
-tr:nth-child(even) {background-color: #f2f2f2}
 </style>
+
 </head>
 <body>
+<div class = "container">
  <img src="pet.png" align="center"  width="51" height="51">
 <table align="center" width= "1000" >
 <h1> Here are all of our visitors! </h1>
@@ -213,6 +166,7 @@ tr:nth-child(even) {background-color: #f2f2f2}
 <th>Address</th>
 <th>E-mail</th>
 <th>Phone Number</th>
+</div>
 
 </tr>
 <?php
@@ -275,6 +229,8 @@ tr:nth-child(even) {background-color: #f2f2f2}
 
     }
 ?>
+
+<br> </br>
 
 </body>
 </html>
