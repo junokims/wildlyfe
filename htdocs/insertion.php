7fa61ea5e6444d0aea45fa161ffebf860e21c4a0
lruiz96 <59980622+lruiz96@users.noreply.github.com>
2020-04-04 13:36:29 -0700

    Update insertion.php

--- htdocs/insertion.php
+++ htdocs/insertion.php
@@ -31,6 +31,7 @@ $sql ="INSERT into AnimalLivesIn (Animal_ID, Enclosure_ID, Species)  VALUES ('$a
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <style>
       html, body {
+        background-image: url("wildlife8.PNG");
       min-height: 100%;
       }
           div, form, input, select, p {
