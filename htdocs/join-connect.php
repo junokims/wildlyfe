2d45269125f17aa854b40fa23a9de6749b9a7df7
lruiz96 <59980622+lruiz96@users.noreply.github.com>
2020-04-04 20:15:55 -0700

    Update join-connect.php
    changed $animalid to $animalspecies just for consistency

--- htdocs/join-connect.php
+++ htdocs/join-connect.php
@@ -1,18 +1,14 @@
-<!-- // include 'animalpage.php';
-// if (isset($_POST['check'])) {
-//     $animalid = $_POST['animal_id'];
-   -->
-  
+ 
 <?php
 include 'animalpage.php';
 if (isset($_POST['check'])) {
-    $animalid = $_POST['species'];
+    $animalspecies = $_POST['species'];
     $dbconnect = mysqli_connect('localhost', 'root', 'root', 'wildlyfe')or die("initial host/db connection problem");
-    $sql = "SELECT food_item_id FROM eats e, animallivesin ali WHERE ali.animal_id=e.animal_id AND ali.species='$animalid'";
+    $sql = "SELECT food_item_id FROM eats e, animallivesin ali WHERE ali.animal_id=e.animal_id AND ali.species='$animalspecies'";
     $result = mysqli_query($dbconnect,$sql);
     if ($result->num_rows > 0) {
     echo "<table>";
-    echo "<tr><th class='border-class'>We feed our $animalid </th></tr>";
+    echo "<tr><th class='border-class'>We feed our $animalspecies</th></tr>";
     while($row = mysqli_fetch_array($result)) {
         $name = $row['food_item_id'];
         echo "<tr><td class='border-class'>".$name."</td></tr>";
